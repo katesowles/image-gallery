ad7290835f892a4cc37feb46e36e10b9f0b46129
Danielle H <daheber@gmail.com>
2016-08-23 13:45:35 -0700

    list view

---
+++ app/src/components/list/list.js
@@ -0,0 +1,12 @@
+import template from './list.html';
+
+export default {
+  template,
+  controller() {
+    this.info = {
+      title: 'Ernie',
+      description: 'Visiting the vet',
+      link: 'https://static1.squarespace.com/static/51911780e4b00c72ce749dc2/t/57bcb321e4fcb567fdff2ee0/1471984444747/?format=1000w'
+    };
+  }
+};
