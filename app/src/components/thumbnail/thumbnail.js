304a4fdf86586cf839f0d69202fab2ea83c0217b
Danielle H <daheber@gmail.com>
2016-08-24 13:35:44 -0700

    refactored with bindings for image info and added album component

--- app/src/components/thumbnail/thumbnail.js
+++ app/src/components/thumbnail/thumbnail.js
@@ -3,8 +3,10 @@ import styles from './thumbnail.scss';
 
 export default{
   template,
+  bindings: {
+    info: '<'
+  },
   controller(){
     this.styles = styles;
-    this.link = 'https://static1.squarespace.com/static/51911780e4b00c72ce749dc2/t/57bcb321e4fcb567fdff2ee0/1471984444747/?format=1000w';
   }
 };
