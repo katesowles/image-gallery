3c4f7ae1a85909ce82e8fab7dfd56f40e395a291
Danielle H <daheber@gmail.com>
2016-08-28 14:45:33 -0700

    added angular material and commented out existing scss

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import components from './components';
+import md from 'angular-material';
 
-const app = angular.module('myApp', [components]);
+const app = angular.module('myApp', [components, md]);
 
 export default app.name;
--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import app from './app';
 import './scss/main.scss';
+import 'angular-material/angular-material.css';
 
 angular.module(app);
 
