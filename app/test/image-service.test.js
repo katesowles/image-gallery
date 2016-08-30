e1214d4253f1d97f97a2b0c1f8d7718bdd5cbfbf
Danielle H <daheber@gmail.com>
2016-08-30 14:25:10 -0700

    service test began

---
+++ app/test/image-service.test.js
@@ -0,0 +1,17 @@
+/* globals angular, chai */
+
+// const {assert} = chai;
+//
+// describe('image service', ()=>{
+//   let $httpBackend = null, imageService = null;
+//
+//   beforeEach(
+//       angular.mock.module( 'services', { apiUrl: '/api' } )
+//   );
+//
+//   beforeEach( angular.mock.inject( ( _imageService_, _$httpBackend_ ) => {
+//     $httpBackend = _$httpBackend_;
+//     imageService = _imageService_;
+//   }));
+//
+// });
