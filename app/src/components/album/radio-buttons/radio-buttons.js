6cd0420290a788118e617cdb493c5716a6d64acc
Danielle H <daheber@gmail.com>
2016-09-03 08:42:16 -0700

    seperated out radio buttons into their own component

---
+++ app/src/components/album/radio-buttons/radio-buttons.js
@@ -0,0 +1,14 @@
+import template from './radio-buttons.html';
+import styles from './radio-buttons.scss';
+
+export default{
+  template,
+  controller,
+  bindings: {
+    display: '='
+  }
+};
+
+function controller(){
+  this.styles = styles;
+}
