bd88571a0b8ad4ad5f5f4c0d2e550b57ebc3cefe
Danielle H <daheber@gmail.com>
2016-09-05 09:32:03 -0700

    update image form html and view in place

---
+++ app/src/components/album/list/update-image-form/update-image-form.js
@@ -0,0 +1,35 @@
+import template from './update-image-form.html';
+import styles from './update-image-form.scss';
+
+export default{
+  template,
+  bindings: {
+    //add: '<',
+    //id: '<'
+  },
+  controller(){
+    this.styles = styles;
+
+    // const resetImage = ()=>{
+    //   this.image = {};
+    // };
+    //
+    // resetImage();
+
+    this.submit = ()=>{
+      //TODO see about refactoring this later
+      // if('title' && 'description' && 'link' in this.image){
+      //   if(this.image.title && this.image.description && this.image.link !== ''){
+      //     this.isInvalid = false;
+      //     this.image.album = this.id;
+      //     this.add(this.image);
+      //     resetImage();
+      //   } else {
+      //     this.isInvalid = true;
+      //   }
+      // } else {
+      //   this.isInvalid = true;
+      // }
+    };
+  }
+};
